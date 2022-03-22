c6e7ac2d44800fef23ff78423535608568b634e9
Lucas-Masaba <masabaluke@gmail.com>
2022-03-23 01:33:34 +0300

    adjust routes.rb according to code review

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "users#index"
-  get "/users", to: "users#index"
-  get "/users/:id", to: "users#show"
-  get "/users/:user_id/posts", to: "posts#index"
-  get "/users/:user_id/posts/:id", to: "posts#show"
+ resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
 
 end
