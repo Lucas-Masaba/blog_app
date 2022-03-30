5edc6c534d842a40623ca002b24f82bf371d3bdb
Lucas-Masaba <masabaluke@gmail.com>
2022-03-30 20:55:29 +0300

    display information from database

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,12 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
 
-  def show; end
+  end
+
+  def show
+    @a_post = Post.find(params[:id])
+    @show_comments = @a_post.comments
+  end
 end
