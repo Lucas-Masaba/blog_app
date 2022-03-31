c9a2375a44296a7290586d37d7ea55375996e96d
Lucas-Masaba <masabaluke@gmail.com>
2022-03-31 20:12:32 +0300

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
   get 'posts/index'
   get 'users/index'
+
+  post 'comments/:post_id'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "users#index"
  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show, :new, :create]
+    resources :comments, only: [:create]
   end
 
 end
