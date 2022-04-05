0fa6458c22a6053c94911a7bf2d35dd441c5aa24
Lucas-Masaba <masabaluke@gmail.com>
2022-04-06 00:53:39 +0300

    add gmail authentification

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,13 +36,27 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
+  config.action_mailer.delivery_method = :smtp
+
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
+  
+  config.action_mailer.smtp_settings = {
+    #user_name:      Rails.application.credentials.dig(:smtp, :user_name),
+    #password:       Rails.application.credentials.dig(:smtp, :password),
+    user_name:      ENV['GMAIL_USERNAME'],
+    password:       ENV['GMAIL_PASS'],
+    domain:         'gmail.com',
+    address:        'smtp.gmail.com',
+    port:           '587',
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+  
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
