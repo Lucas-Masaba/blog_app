0b53c67ce1b1bf17fb414f75793231dd75f36652
TheLuisAbarca <arclaav.adm@gmail.com>
2022-04-08 10:27:53 -0500

    Add a namespace in order to mantain enable the functionality that we already have implemented and also use the new one that is API.

--- config/routes.rb
+++ config/routes.rb
@@ -11,10 +11,17 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "users#index"
- resources :users, only: [:index, :show] do
+  resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create, :destroy] do
       resources :comments, only: [:destroy]
     end
   end
 
+  namespace :api, defaults: { format: :json} do
+    post 'users/register', to: 'authentication#register'
+    post 'users/login', to: 'authentication#login'
+    get 'posts', to: 'api#posts_index'
+    get 'comments', to: 'api#list_comments'
+    post 'new_comment', to: 'api#create_comment'
+  end
 end
