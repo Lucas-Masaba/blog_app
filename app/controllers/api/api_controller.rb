4dd70e12665f59db6b0abb45a50f5b81d7c9e0db
TheLuisAbarca <arclaav.adm@gmail.com>
2022-04-08 10:31:58 -0500

    Add controllers to allow us to make authentications through API.

---
+++ app/controllers/api/api_controller.rb
@@ -0,0 +1,28 @@
+module Api
+    class ApiController < ApplicationController
+      skip_before_action :verify_authenticity_token
+  
+      def posts_index
+        @user = User.find_by_api_token(request.headers['X-AUTH-TOKEN'])
+        @posts = Post.where(user_id: @user.id)
+        render json: @posts
+      end
+  
+      def list_comments
+        @user = User.find_by_api_token(request.headers['X-AUTH-TOKEN'])
+        @comments = Comment.where(user_id: @user_id)
+        render json: @comments
+      end
+  
+      def create_comment
+        @user = User.find_by_api_token(request.headers['X-AUTH-TOKEN'])
+        @new_comment = @user.comments.new(post_id: params[:post_id], text: params[:text])
+  
+        if @new_comment.save
+          render json: @new_comment, status: :created
+        else
+          render json: @new_comment.errors, status: :unprocessable_entity
+        end
+      end
+    end
+  end
